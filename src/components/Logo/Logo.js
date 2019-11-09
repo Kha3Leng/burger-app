58c34edb0d0221a1372b715df6d1550ff00711ac
Sai Khae Leang <sai@Sais-MacBook-Pro.local>
2019-11-09 21:01:40 +0630

    added all

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,7 +4,7 @@ import classes from './Logo.css';
 import burgerLogo from '../../assets/images/burger.png';
 const logo = props => (
     <div className={classes.Logo}>
-        <img src={burgerLogo} />
+        <img src={burgerLogo} alt="My Burger App" />
     </div>
 );
 
