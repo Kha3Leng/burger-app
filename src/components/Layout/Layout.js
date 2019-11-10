c34f2b51040b24bb4b18c57f2a20dc75f6a08f29
Sai Khae Leang <sai@Sais-MacBook-Pro.local>
2019-11-10 18:35:00 +0630

    mobile responsive now

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import Aux from '../../hoc/Aux';
+import Sidebar from '../Navigation/Sidebar/Sidebar';
 import Classes from './Layout.css';
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <main className={Classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
+    state = {
+        showDrawer: false
+    }
 
-export default layout;
+    showDrawerHandler = () => {
+        this.setState({ showDrawer: false });
+    };
+
+    drawerToggleHander = () => {
+        this.setState((prevState, props) => { return { showDrawer: !prevState.showDrawer } });
+    };
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar showDrawerToggle={this.drawerToggleHander} />
+                <Sidebar open={this.state.showDrawer} showDrawer={this.showDrawerHandler} />
+                <main className={Classes.Content}>
+                    {this.props.children}
+                </main>
+
+            </Aux>
+        );
+
+    }
+}
+export default Layout;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,12 +3,15 @@ import React from 'react';
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../Sidebar/DrawerToggle/DrawerToggle';
 
 const toolbar = props => (
     <header className={classes.Toolbar}>
-        <div>MENU</div>
-        <Logo />
-        <nav>
+        <DrawerToggle toggle={props.showDrawerToggle} />
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
+        <nav className={classes.DesktopOnly}>
             <NavigationItems />
         </nav>
     </header>
